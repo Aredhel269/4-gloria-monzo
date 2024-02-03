d8dcd9e1737958d3728b8dc68953462442c87a0e
Glòria Monzó <dark.aredhel@gmail.com>
2024-02-03 13:26:13 +0100

    end database

--- src/domain/usecases/taskRepository.ts
+++ src/domain/usecases/taskRepository.ts
@@ -3,6 +3,6 @@ import { Task } from '../entities/task'
 export interface TaskRepository {
     createTask (task: Task): Task
     completeTask (id: number): void
-    deleteTask (id: number): null
+    deleteTask (id: number): void
     listTask (): Task[]
 }
