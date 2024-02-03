c9ef95d9961bf248e945cef1fb124b4e9dc973fc
Glòria Monzó <dark.aredhel@gmail.com>
2024-02-03 12:16:34 +0100

    my first commit

---
+++ src/domain/usecases/taskRepository.ts
@@ -0,0 +1,8 @@
+import { Task } from '../entities/task'
+
+export interface TaskRepository {
+    createTask (task: Task): Task
+    completeTask (id: number): void
+    deleteTask (id: number): null
+    listTask (): Task[]
+}
